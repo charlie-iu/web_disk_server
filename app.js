e641cdd6ab9b10b285f378837e43b3c1197fdb5c
charlie <1084906272@qq.com>
2023-03-08 18:54:11 +0800

    config cors

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3001"
+}));
+
 app.use(express.json());
 app.use('/api', router);
 
 
 // 监听3000端口
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('服务器已启动');
 });
 
